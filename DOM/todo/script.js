096b812731ca7ae930987daa0696ea1ebdea4ae5
Saurabh Bansal <saurabhbansal2443@gmail.com>
2023-12-06 19:00:02 +0530

    todo list completed

--- DOM/todo/script.js
+++ DOM/todo/script.js
@@ -1,14 +1,32 @@
-let input = document.querySelector(".in");
-let btn = document.querySelector(".btn");
+// let input = document.querySelector(".in");
+// let btn = document.querySelector(".btn");
 
-let cont = document.querySelector(".cont");
+// let cont = document.querySelector(".cont");
+
+// btn.addEventListener("click",function(){
+//     let task = input.value ;
+//         input.value="";
+//     let myele  = document.createElement("li");
+
+//     myele.innerHTML=task;
+
+//     cont.appendChild(myele);
+// })
+
+let inputTag = document.querySelector(".in");
+let btn = document.querySelector(".btn");
+let ultag = document.querySelector(".ul");
 
 btn.addEventListener("click",function(){
-    let task = input.value ;
-        input.value="";
-    let myele  = document.createElement("li");
+    let task = inputTag.value;
+    inputTag.value="";
+    console.log(task);
+    let myele = document.createElement("li");
+    myele.innerHTML = task + "<button class='delbtn'>delete</button>";
 
-    myele.innerHTML=task;
-
-    cont.appendChild(myele);
+    let delBtn = myele.querySelector(".delbtn");
+    delBtn.addEventListener("click",function(){
+        myele.remove();
+    })
+    ultag.appendChild(myele);
 })
